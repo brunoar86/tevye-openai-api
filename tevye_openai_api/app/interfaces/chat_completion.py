963dcbda4ec734d8f8ea784c7f49808332a71cc1
brunoar86 <ribeiro256@msn.com>
2025-07-12 21:11:48 -0300

    feat: chat completions/web_search_options, top_p and tools

--- tevye_openai_api/app/interfaces/chat_completion.py
+++ tevye_openai_api/app/interfaces/chat_completion.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 class ChatCompletionRequest(BaseModel):
     model: str
-    messages: Optional[list] = None
+    messages: list[object] = None
     frequency_penalty: Optional[float] = 0.0
     logit_bias: Optional[dict] = None
     logprobs: Optional[bool] = False
@@ -15,3 +15,11 @@ class ChatCompletionRequest(BaseModel):
     prediction: Optional[dict] = None
     presence_penalty: Optional[float] = 0.0
     reasoning_effort: Optional[dict] = None
+    response_format: Optional[dict] = None
+    stream: Optional[bool] = False
+    temperature: Optional[float] = 1.0
+    tool_choice: Optional[str | dict] = None
+    tools: Optional[list] = None
+    top_logprobs: Optional[int] = None
+    top_p: Optional[float] = 1.0
+    web_search_options: Optional[dict] = None
--- tevye_openai_api/app/modules/chat_completion.py
+++ tevye_openai_api/app/modules/chat_completion.py
@@ -10,19 +10,7 @@ class ChatCompletion:
         self.OPENAI_API_KEY = project.key()
         self.PROJECT_ID = project.id()
         self.ORGANIZATION_ID = project.organization()
-        self.model = None
-        self.messages = None
-        self.audio = None
-        self.frequency_penalty = 0.0
-        self.logit_bias = None
-        self.logprobs = False
-        self.max_completion_tokens = 100
-        self.modalities = None
-        self.n = 1
-        self.parallel_tool_calls = None
-        self.prediction = None
-        self.presence_penalty = 0
-        self.reasoning_effort = None
+        self.input = None
 
     def assemble_request(self):
         url = 'https://api.openai.com/v1/chat/completions'
@@ -34,25 +22,11 @@ class ChatCompletion:
             'OpenAI-Organization': self.ORGANIZATION_ID
         }
 
-        payload = {
-            'model': self.model,
-            'messages': self.messages,
-            'frequency_penalty': self.frequency_penalty,
-            'logit_bias': self.logit_bias,
-            'logprobs': self.logprobs,
-            'modalities': self.modalities,
-            'n': self.n,
-            'presence_penalty': self.presence_penalty
-        }
+        payload = {}
 
-        if self.parallel_tool_calls is not None:
-            payload['parallel_tool_calls'] = self.parallel_tool_calls
-
-        if self.prediction is not None:
-            payload['prediction'] = self.prediction
-
-        if self.reasoning_effort is not None:
-            payload['reasoning_effort'] = self.reasoning_effort
+        for k, v in dict(self.input).items():
+            if k not in payload and v is not None:
+                payload[str(k)] = v
 
         req = {
             'url': url,
@@ -63,14 +37,19 @@ class ChatCompletion:
         return req
 
     def assemble_response_data(self, response):
-        self.messages.append(response['choices'][0]['message'])
-        self.model = self.set_model(response['model'])
-        if self.logprobs is True:
-            self.logprobs = response['choices'][0]['logprobs']
+        model = None
+        messages = []
+        logprobs = None
+
+        messages.append(response['choices'][0]['message'])
+        model = response['model']
+        if 'logprobs' in response['choices'][0]:
+            logprobs = response['choices'][0]['logprobs']
+
         data = {
-            'model': self.model,
-            'messages': self.messages,
-            'logprobs': self.logprobs
+            'model': model,
+            'messages': messages,
+            'logprobs': logprobs
         }
 
         return data
@@ -82,21 +61,7 @@ class ChatCompletion:
 
     async def request(self, input):
 
-        self.set_model(input.model)
-        self.set_messages(input.messages)
-        self.set_frequency_penalty(input.frequency_penalty)
-        self.set_logit_bias(input.logit_bias, input.model)
-        self.set_logprobs(input.logprobs)
-        self.set_max_completion_tokens(input.max_completion_tokens)
-        self.set_modalities(input.modalities)
-        self.set_n(input.n)
-        if input.parallel_tool_calls is not None:
-            self.set_parallel_tool_calls(input.parallel_tool_calls)
-        self.set_presence_penalty(input.presence_penalty)
-        if input.prediction is not None:
-            self.set_prediction(input.prediction)
-        if input.reasoning_effort is not None:
-            self.set_reasoning_effort(input.reasoning_effort)
+        self.input = input
 
         req = self.assemble_request()
 
@@ -114,49 +79,18 @@ class ChatCompletion:
             except aiohttp.ClientError as error:
                 print('Erro: {}'.format(error))
 
-    def set_model(self, model: str):
-        self.model = model
-
-    def set_messages(self, messages: list):
-        self.messages = messages
-
-    def set_frequency_penalty(self, frequency_penalty: float):
-        self.frequency_penalty = frequency_penalty
-
-    def set_logit_bias(self, logit_bias: dict, model: str):
+    def map_logit_bias(self, logit_bias: dict, model: str):
         if logit_bias is None:
             pass
         else:
-            self.logit_bias = dict()
+            logit_bias = dict()
             encoder = tiktoken.encoding_for_model(model)
             for k, v in logit_bias.items():
                 tokens = encoder.encode(k)
                 if len(tokens) > 1:
                     for token in tokens:
-                        self.logit_bias[str(token)] = v
+                        logit_bias[str(token)] = v
                 else:
-                    self.logit_bias[str(tokens[0])] = v
+                    logit_bias[str(tokens[0])] = v
 
-    def set_logprobs(self, logprobs: bool):
-        self.logprobs = logprobs
-
-    def set_max_completion_tokens(self, max_completion_tokens):
-        self.max_completion_tokens = max_completion_tokens
-
-    def set_modalities(self, modalities):
-        self.modalities = modalities
-
-    def set_n(self, n):
-        self.n = n
-
-    def set_parallel_tool_calls(self, parallel_tool_calls):
-        self.parallel_tool_calls = parallel_tool_calls
-
-    def set_prediction(self, prediction):
-        self.prediction = prediction
-
-    def set_presence_penalty(self, presence_penalty):
-        self.presence_penalty = presence_penalty
-
-    def set_reasoning_effort(self, reasoning_effort):
-        self.reasoning_effort = reasoning_effort
+        return logit_bias
